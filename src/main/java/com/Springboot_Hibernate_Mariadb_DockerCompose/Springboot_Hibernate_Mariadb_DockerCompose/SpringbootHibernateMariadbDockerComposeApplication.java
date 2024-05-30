e828ecf62d8819661d81acaf36b5f493d7229209
Vivek1099 <viveksawaiyan8@gmail.com>
2024-05-30 22:41:14 +0530

    Docker compose demo

---
+++ src/main/java/com/Springboot_Hibernate_Mariadb_DockerCompose/Springboot_Hibernate_Mariadb_DockerCompose/SpringbootHibernateMariadbDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.Springboot_Hibernate_Mariadb_DockerCompose.Springboot_Hibernate_Mariadb_DockerCompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateMariadbDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateMariadbDockerComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Springboot_Hibernate_Mariadb_DockerCompose/Springboot_Hibernate_Mariadb_DockerCompose/SpringbootHibernateMariadbDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot_Hibernate_Mariadb_DockerCompose.Springboot_Hibernate_Mariadb_DockerCompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateMariadbDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
